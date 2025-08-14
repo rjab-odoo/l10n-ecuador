3a57168b4a837948b1cc0ade561bc249c9fa26d4
Ricardo Altamirano <39317318+rjab-odoo@users.noreply.github.com>
2025-08-14 00:07:43 -0500

    Update __init__.py
    Misstake a letter "l"

--- l10n_ec_base/__init__.py
+++ l10n_ec_base/__init__.py
@@ -6,4 +6,4 @@ from odoo import api, SUPERUSER_ID
 
 def _l10n_ec_base_post_init(cr, registry):
     env = api.Environment(cr, SUPERUSER_ID, {})
-    env["account.chart.template"]._10n_ec_post_init()
+    env["account.chart.template"]._l10n_ec_post_init()
